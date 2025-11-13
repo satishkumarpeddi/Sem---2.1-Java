278d62ec3ec855d61abc4b3d7da721a3edeee87f
evhook <anonymous1986222@gmail.com>
2025-11-13 20:23:08 +0530

    Add initial implementation of classes A and B for demonstration of multiple inheritance

---
+++ MultipleInheritanceInJavaUsingClass/Demo.java
@@ -0,0 +1,20 @@
+package MultipleInheritanceInJavaUsingClass;
+
+class A{
+    void display(){
+        System.out.println("Class A display method . ");
+    }
+}
+class B{
+    void display(){
+        System.out.println("Class B display method . ");
+    }
+}
+// class C extends A,B{
+
+// } Class Diamond Problem 
+public class Demo {
+    public static void main(String[] args){
+        
+    }
+}
