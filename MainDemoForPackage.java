2852092b79e61fcd11bbffe883e5dda57b16ff7a
evhook <anonymous1986222@gmail.com>
2025-11-13 19:57:13 +0530

    Add MainDemoForPackage and Addition classes for basic addition functionality

---
+++ MainDemoForPackage.java
@@ -0,0 +1,7 @@
+import packageDemo.*;
+public class MainDemoForPackage {
+    public static void main(String[] args){
+        Addition add = new Addition();
+        add.sum(10,20);
+    }
+}
---
+++ packageDemo/Addition.java
@@ -0,0 +1,6 @@
+package packageDemo;
+public class Addition{
+    public static void sum(int a,int b){
+        System.out.println("The sum of two number : "+(a+b));
+    }
+}
